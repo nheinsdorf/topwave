091d39cc131b441d9c1f1bf440f49b084c5c77a4
Niclas Heinsdorf <nheinsdorf@gmail.com>
2022-07-05 14:44:18 +0900

    Added setup.py using PyCharms integrated tool.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='topwave',
+    version='0.1.0',
+    packages=['tests', 'topwave'],
+    url='',
+    license='',
+    author='Niclas Heinsdorf',
+    author_email='',
+    description=''
+)
