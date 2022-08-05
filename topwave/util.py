52237c23f854b4524ffd98ba011c9cd889de2ff1
Niclas Heinsdorf <nheinsdorf@gmail.com>
2022-08-04 17:25:01 -0700

    Added 'gaussian' to util.py

--- topwave/util.py
+++ topwave/util.py
@@ -3,6 +3,47 @@ from numpy.linalg import norm
 
 from topwave.model import Model
 
+def bose_distribution(energies, temperature):
+    """Calculates the Bose-Einstein distribution for a given set of energies and a temperature.
+
+    Parameters
+    ----------
+    energies : float or numpy.ndarray
+        Energies for which the distribution is evaluated.
+    temperature : float
+        The temperature given in Kelvin.
+
+    Returns
+    -------
+    A numpy.ndarray with the Bose-Einstein distribution for each energy.
+
+    """
+
+    energies = np.array([energies], dtype=float).flatten()
+    # NOTE: build in a check for zero energy?!
+    if temperature == 0:
+        return np.zeros(energies.shape, dtype=float)
+    else:
+        return np.reciprocal(np.exp(energies / (Model.kB * temperature)) - 1)
+
+def gaussian(x, mean, std):
+    """Evaluates the Gauss distribution at x.
+
+    Parameters
+    ----------
+    x : float or numpy.ndarray
+        The values at which the Gaussian distribution should be evaluated.
+    mean : float
+        The mean value of the distribution.
+    std : float
+        The standard deviation of the distribution.
+
+    """
+
+    x = np.array([x], dtype=float).flatten()
+    return np.exp(-0.5 * np.square((x - mean) / std)) / (std * np.sqrt(2 * np.pi))
+
+
 def rotate_vector_to_ez(v):
     """Creates a 3x3 rotation matrix R with R v = [0, 0, 1]
 
@@ -29,26 +70,5 @@ def rotate_vector_to_ez(v):
 
     return np.array([e1, e2, e3]).T
 
-def bose_distribution(energies, temperature):
-    """Calculates the Bose-Einstein distribution for a given set of energies and a temperature.
 
-    Parameters
-    ----------
-    energies : float or numpy.ndarray
-        Energies for which the distribution is evaluated.
-    temperature : float
-        The temperature given in Kelvin.
-
-    Returns
-    -------
-    A numpy.ndarray with the Bose-Einstein distribution for each energy.
-
-    """
-
-    energies = np.array([energies], dtype=float).flatten()
-    # NOTE: build in a check for zero energy?!
-    if temperature == 0:
-        return np.zeros(energies.shape, dtype=float)
-    else:
-        return np.reciprocal(np.exp(energies / (Model.kB * temperature)) - 1)
 
