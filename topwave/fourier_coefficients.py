Fixed generation of derivatives of hamiltonian

topwave/fourier_coefficients.py
@@ -2,12 +2,34 @@ from __future__ import annotations
 
 
 import numpy as np
+from pymatgen.core.sites import PeriodicSite
 
 from topwave.coupling import Coupling
 from topwave.types import ComplexList, VectorList
 
 
 #TODO: implement the other convention of FT that takes into account the intra cell positions of sites.
+def get_intracell_fourier_coefficient(site: PeriodicSite,
+                                      kpoint: VectorList) -> ComplexList:
+    """Returns the phase factor from different sublattice positions.
+
+
+
+    Parameters
+    ----------
+    site: PeriodicSite
+        The `pymatgen.core.sites.PeriodicSite` for which the coefficient is computed.
+    kpoint: VectorList
+        The kpoints at which the coefficient is computed.
+
+    Returns
+    -------
+    ComplexList
+        The intracell Fourier coefficient at the given k-point of the site.
+
+    """
+
+    return np.exp(-2j * np.pi * np.einsum('x, kx -> k', site.frac_coords, kpoint))
 
 def get_periodic_fourier_coefficient(coupling: Coupling,
                                      kpoint: VectorList) -> ComplexList:
@@ -23,7 +45,7 @@ def get_periodic_fourier_coefficient(coupling: Coupling,
     coupling: Coupling
         The `topwave.coupling.Coupling` for which the coefficient is computed.
     kpoint: VectorList
-        The kpoint at which the coefficient is computed.
+        The kpoints at which the coefficient is computed.
 
     Returns
     -------
@@ -49,7 +71,7 @@ def get_periodic_fourier_derivative(coupling: Coupling,
     coupling: Coupling
         The `topwave.coupling.Coupling` for which the derivative is computed.
     kpoint: VectorList
-        The kpoint at which the derivative is computed.
+        The kpoints at which the derivative is computed.
     direction: str
         Which crystal momentum is used for the derivative. Options are 'x', 'y' and 'z'.
 

topwave/response.py
@@ -2,22 +2,57 @@ from __future__ import annotations
 
 import numpy as np
 import numpy.fft as fft
-from topwave.types import SquareMatrixList
 
+from topwave.constants import K_BOLTZMANN
+from topwave.fourier_coefficients import get_intracell_fourier_coefficient
+from topwave.model import TightBindingModel
+from topwave.set_of_kpoints import Grid
+from topwave.spec import Spec
+from topwave.types import ListOfRealList
+from topwave.util import fermi_distribution
+
+
+def get_nernst_conductivity(
+        berry_curvature: ListOfRealList,
+        energies: ListOfRealList,
+        filling: float,
+        temperature: float) -> float:
+    """Computes the Nernst Conductivity.
+
+    Parameters
+    ----------
+    berry_curvature: ListOfRealList
+        The Berry Curvature at each k-point for all bands.
+    energies: ListOfRealList
+        The energies at each k-point for all bands.
+    filling: float
+        The energy up to which the states are considered.
+    temperature: float
+        The temperature.
+
+    Returns
+    -------
+    float
+        The Nernst conductivity at some filling.
+
+    """
+
+    energies * fermi_distribution(energies, temperature) + K_BOLTZMANN * temperature * np.log(1 + np.exp(energies - filling))
 
 def get_bare_susceptibility(
-        hamiltonians: SquareMatrixList,
+        model: TightBindingModel,
         k_grid_shape: tuple[int, int, int],
         temperature: float,
-        num_matsubara_frequencies: int) -> np.ndarray[np.float64]:
-    """Computes the bare susceptibility tensor for a given spectrum.
+        num_matsubara_frequencies: int,
+        symmetrize: bool = True) -> np.ndarray[np.float64]:
+    """Computes the bare susceptibility tensor for a given model.
 
     This uses the imaginary time representation to efficiently calculate the product of Green's functions.
     Cite Something
 
     Parameters
     ----------
-    hamiltonians: SquareMatrixList
+    model: TightBindingModel
         A list of tight-binding hamiltonians on a grid that covers the Brillouin zone that is used to calculate the bare
         suscpetibility. The shape should be the shape of the grid times the dimension of the hamiltonians.
     k_grid_shape: tuple(int, int, int)
@@ -30,6 +65,9 @@ def get_bare_susceptibility(
     num_matsubara_frequencies: int
         The number of matsubara frequencies. Increase this number until convergence is reached. A good starting point
         is 256.
+    symmetrize: bool
+        If true, the susceptibility tensor is transformed taking into account the intracell sublattice positions to
+        restore the symmetries of e.g. nonsymmorphic crystals. Default is true.
 
     Returns
     -------
@@ -65,7 +103,10 @@ def get_bare_susceptibility(
 
     """
 
-    hamiltonians = np.array(hamiltonians, dtype=np.complex128)
+    grid = Grid(num_x=k_grid_shape[0], num_y=k_grid_shape[1], num_z=k_grid_shape[2],
+                x_min=0, x_max=1, y_min=0, y_max=1, z_min=0, z_max=1,
+                endpoint_x=False, endpoint_y=False, endpoint_z=False)
+    hamiltonians = Spec.get_tightbinding_hamiltonian(model, grid)
     num_bands = hamiltonians.shape[-1]
     hamiltonians = hamiltonians.reshape((*k_grid_shape, num_bands, num_bands))
 
@@ -90,10 +131,22 @@ def get_bare_susceptibility(
                                                       1, axis=2),
                                               1, axis=3)
     Greens_functions_negative_times[0] *= -1
-    product_Greens_functions = -np.einsum('fhklmn, fhklop -> fhklmnop', Greens_functions, Greens_functions_negative_times)
+    product_Greens_functions = -np.einsum('fhklmn, fhklop -> fhklmnop',
+                                          Greens_functions,
+                                          Greens_functions_negative_times) / temperature
     susceptibility = (fft.fftn(fft.ifft(product_Greens_functions, axis=0)
                                , axes=[1, 2, 3]))
+    if not symmetrize:
+        return  susceptibility
 
-    return susceptibility
+    intracell_phase_factors = np.array([get_intracell_fourier_coefficient(site, grid.kpoints)
+                                        for site in model.structure], dtype=np.complex128).T
+    intracell_phase_factors = intracell_phase_factors.reshape((*grid.shape, num_bands))[..., np.newaxis] \
+                              * np.eye(len(model.structure))
+    symmetric_susceptibility = np.einsum('whklabcd, hklbc, hklad -> whklabcd',
+                                         fft.fftshift(susceptibility, axes=[1, 2, 3]),
+                                         intracell_phase_factors,
+                                         np.conj(intracell_phase_factors))
+    return symmetric_susceptibility
 
 

topwave/spec.py
@@ -149,9 +149,8 @@ class Spec:
     @staticmethod
     def get_tightbinding_hamiltonian(
             model: TightBindingModel,
-            kpoints: VectorList | SetOfKPoints,
-            derivative: str = None) -> SquareMatrix:
-        """Constructs the Tight Binding Hamiltonian (or its derivative) for a set of given k-points.
+            kpoints: VectorList | SetOfKPoints) -> SquareMatrix:
+        """Constructs the Tight Binding Hamiltonian for a given set of k-points.
 
 
         Parameters
@@ -160,23 +159,18 @@ class Spec:
             The model of that is used to calculate the spectrum.
         kpoints: VectorList | SetOfKPoints
             The kpoints at which the Hamiltonian or its derivatives are constructed.
-        derivative: str
-            String that indicates which derivative should be constructed. Options are 'x', 'y' or 'z'.
-            Default is None.
 
         Returns
         -------
         SquareMatrix
-            The Hamiltonian or one of its derivatives.
+            The Hamiltonian.
 
         """
 
         kpoints = format_kpoints(kpoints)
 
-        if derivative is None:
-            k_dependence = get_periodic_fourier_coefficient
-        else:
-            k_dependence = partial(get_periodic_fourier_derivative, direction=derivative)
+        k_dependence = get_periodic_fourier_coefficient
+
         matrix = np.zeros((len(kpoints), len(model.structure), len(model.structure)), dtype=complex)
 
         # construct matrix elements at each k-point
@@ -213,6 +207,58 @@ class Spec:
 
         return matrix
 
+    @staticmethod
+    def get_tightbinding_derivative(
+            model: TightBindingModel,
+            kpoints: VectorList | SetOfKPoints,
+            derivative: str) -> SquareMatrix:
+        """Constructs the derivative of a Tight Binding Hamiltonian for a given set of k-points.
+
+
+        Parameters
+        ----------
+        model: TightBindingModel
+            The model of that is used to calculate the spectrum.
+        kpoints: VectorList | SetOfKPoints
+            The kpoints at which the Hamiltonian or its derivatives are constructed.
+        derivative: str
+            String that indicates which derivative should be constructed. Options are 'x', 'y' or 'z'.
+
+        Returns
+        -------
+        SquareMatrix
+            The derivative of the Hamiltonian.
+
+        """
+
+        kpoints = format_kpoints(kpoints)
+
+        k_dependence = partial(get_periodic_fourier_derivative, direction=derivative)
+        matrix = np.zeros((len(kpoints), len(model.structure), len(model.structure)), dtype=complex)
+
+        # construct matrix elements at each k-point
+        # for _, kpoint in enumerate(kpoints):
+        for coupling in model.get_set_couplings():
+            i, j = coupling.site1.properties['index'], coupling.site2.properties['index']
+
+            # get the matrix elements from the couplings
+            # A, inner = coupling.get_tightbinding_matrix_elements(kpoint)
+            A = k_dependence(coupling, kpoints) * coupling.get_tightbinding_matrix_elements()
+            matrix[:, i, j] += A
+            matrix[:, j, i] += np.conj(A)
+
+        # add spin degrees of freedom
+        if model.check_if_spinful():
+            matrix = np.kron(matrix, np.eye(2))
+
+            for coupling in model.get_set_couplings():
+                i, j = coupling.site1.properties['index'], coupling.site2.properties['index']
+                spin_orbit_term = np.einsum('c, nm -> cnm', k_dependence(coupling, kpoints), coupling.get_spin_orbit_matrix_elements())
+                matrix[:, 2 * i:2 * i + 2, 2 * j:2 * j + 2] += spin_orbit_term
+                matrix[:, 2 * j:2 * j + 2, 2 * i:2 * i + 2] += np.conj(spin_orbit_term.swapaxes(1, 2))
+
+        return matrix
+
     def solve(self, solver: Callable[[npt.NDArray[np.complex128]], tuple[npt.NDArray[np.complex128], npt.NDArray[np.complex128]]]) -> tuple[npt.NDArray[np.complex128], npt.NDArray[np.complex128]]:
         """Diagonalizes the Hamiltonian using the provided solver."""
 
@@ -241,7 +287,9 @@ class Spec:
         return E, psi
 
 
-    def get_berry_phase(self, occupied: IntVector) -> float:
+    def get_berry_phase(self,
+                        band_indices: IntVector = None,
+                        energy: float = None) -> float:
         """Computes the Berry phase.
 
         See Also
@@ -250,7 +298,7 @@ class Spec:
 
         """
 
-        loop_operator = get_fermionic_wilson_loop(self, occupied)
+        loop_operator = get_fermionic_wilson_loop(self, band_indices, energy)
         return get_berry_phase(loop_operator)
 
     def get_spectral_density(self,

topwave/topology.py
@@ -29,12 +29,12 @@ def get_berry_curvature(
     """
 
     index1, index2 = get_berry_curvature_indices(component)
-    partial_derivative_matrix1 = spec.get_tightbinding_hamiltonian(spec.model,
-                                                                   spec.kpoints.kpoints,
-                                                                   derivative='xyz'[index1])
-    partial_derivative_matrix2 = spec.get_tightbinding_hamiltonian(spec.model,
-                                                                   spec.kpoints.kpoints,
-                                                                   derivative='xyz'[index2])
+    partial_derivative_matrix1 = spec.get_tightbinding_derivative(spec.model,
+                                                                  spec.kpoints.kpoints,
+                                                                  derivative='xyz'[index1])
+    partial_derivative_matrix2 = spec.get_tightbinding_derivative(spec.model,
+                                                                  spec.kpoints.kpoints,
+                                                                  derivative='xyz'[index2])
 
     num_bands = spec.energies.shape[1]
 
@@ -82,11 +82,15 @@ def get_bosonic_wilson_loop(spectrum: Spec, band_indices: IntVector) -> Matrix:
 
     pass
 
-def get_fermionic_wilson_loop(spectrum: Spec, band_indices: IntVector) -> Matrix:
+def get_fermionic_wilson_loop(spectrum: Spec,
+                              band_indices: IntVector = None,
+                              energy: float = None) -> Matrix:
     """Constructs the Wilson loop operator of a fermionic spectrum.
 
     For a spectrum at Nk k-points, the inner product of Nk - 1 eigenfunctions for a given selection of
-    states is evaluated. The ordering is the same as that of the k-points.
+    states is evaluated. The ordering is the same as that of the k-points. The states can be selected by providing a
+    list of band indices, or all states below some energy can be selected. In that case the wilson loop operator is
+    a product of rectangular matrices.
 
     .. admonition:: Careful!
         :class: warning
@@ -100,14 +104,17 @@ def get_fermionic_wilson_loop(spectrum: Spec, band_indices: IntVector) -> Matrix
     spectrum: Spec
         The spectrum that contains the eigenfunctions of the model.
     band_indices: IntVector
-        List of band indices that are selected to compute the Wilson loop operator.
+        List of band indices that are selected to compute the Wilson loop operator. If None, all states are selected.
+    energy: float
+        If not None, only states below energy are used to compute the Wilson loop. Default is None.
 
     """
 
-    psi = spectrum.psi[:, :, band_indices]
-
+    band_indices = np.arange(spectrum.energies.shape[1]) if band_indices is None else np.array(band_indices, dtype=np.int64)
+    energies, psi = spectrum.energies[:, band_indices], spectrum.psi[:, :, band_indices]
     # check whether start and end k-point are the same and impose closed loop
 
+    # should I check this or give resonsibility to user?
     if np.all(np.isclose(spectrum.kpoints.kpoints[0], spectrum.kpoints.kpoints[-1])):
         psi[0] = psi[-1]
     else:
@@ -116,13 +123,31 @@ def get_fermionic_wilson_loop(spectrum: Spec, band_indices: IntVector) -> Matrix
         # see 'impose_pbc'-method
         pass
 
-    # construct bra-eigenvectors for k+1
-    psi_left = np.roll(np.conj(psi), 1, axis=0)
+    # standard
+    if energy is None:
+        # construct bra-eigenvectors for k+1
+        psi_left = np.roll(np.conj(psi), 1, axis=0)
 
-    # compute num_k - 1 overlaps
-    loop = np.einsum('knm, knl -> kml', psi_left[1:], psi[1:])
+        # compute num_k - 1 overlaps
+        loop = np.einsum('knm, knl -> kml', psi_left[1:], psi[1:])
 
-    # do the SVD cleanup?
-    # take the product of the matrices to compute the wilson loop operator
-    return np.linalg.multi_dot(loop)
+        # do the SVD cleanup?
+        # take the product of the matrices to compute the wilson loop operator
+        return np.linalg.multi_dot(loop)
+
+    # Ken's method
+    psi_left = np.conj(psi[0, :, energies[0] <= energy].T)
+    psi_right = psi[1, :, energies[1] <= energy].T
+    product = np.einsum('nm, nl -> ml', psi_left, psi_right)
+    print(product.shape)
+    for k_index, kpoint in enumerate(spectrum.kpoints.kpoints[1:-1]):
+        psi_left = np.conj(psi[k_index + 1, :, energies[k_index + 1] <= energy].T)
+        psi_right = psi[k_index + 2, :, energies[k_index + 2] <= energy].T
+        intermediate = np.einsum('nm, nl -> ml', psi_left, psi_right)
+        product = product @ intermediate
+    if product.size == 0:
+        return [[0]]
+    return product
+
+
 